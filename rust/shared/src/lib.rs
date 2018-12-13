b10fe1a8cb6c7bc8df192bb4e6a342fcc6c852c1
risboo6909 <risboo6909@yandex-team.ru>
2018-12-13 19:04:57 +0300

    Apply "cargo fmt"

--- rust/shared/src/lib.rs
+++ rust/shared/src/lib.rs
@@ -1,9 +1,9 @@
 use std::io::{self, BufRead};
 
-
-pub fn parse_input<F, T>(func: F) -> Vec<T> 
-    where F: Fn(&str) -> T {
-
+pub fn parse_input<F, T>(func: F) -> Vec<T>
+where
+    F: Fn(&str) -> T,
+{
     let mut output = Vec::new();
     for line in io::stdin().lock().lines() {
         let val = line.unwrap();
